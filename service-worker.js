16c27ddb1795d898ab9001a880251d0433886211
sbpublicschoolchhitauna <faebookmonitization9044@gmail.com>
2025-12-19 20:22:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,34 @@
+const CACHE_NAME = 'sbpsc-pwa-v2';
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
