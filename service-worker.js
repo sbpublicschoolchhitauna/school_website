4ed8fa66a0fa86fdd37db3369e211b91bcaa85fd
sbpublicschoolchhitauna <faebookmonitization9044@gmail.com>
2025-12-22 22:29:03 +0530

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="sbpsc-icon-v9";
+const CACHE_NAME="sbpsc-color-v9";
 self.addEventListener("install",e=>self.skipWaiting());
 self.addEventListener("activate",e=>{
  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==CACHE_NAME&&caches.delete(x)))));
