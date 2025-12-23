f78f1ea6c98249adba7284008bea84bf0afbfb0b
sbpublicschoolchhitauna <faebookmonitization9044@gmail.com>
2025-12-24 00:39:21 +0530

    Update

--- greeting/name-confirm.js
+++
@@ -1,8 +0,0 @@
-
-function confirmStudent(name,cls){
- if(confirm(`क्या आप यही छात्र हैं?\nनाम: ${name}\nClass: ${cls}`)){
-  localStorage.studentName=name;
- }else{
-  localStorage.removeItem("studentName");
- }
-}
